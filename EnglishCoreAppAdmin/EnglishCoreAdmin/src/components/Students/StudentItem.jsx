67bff660d3bc46e7ea2219e64ad90f58bdb37ad5
danyloagd <daniellopezaguilera1310@gmail.com>
2024-12-27 16:29:59 -0600

    Visual estudiantes inicio

---
+++ EnglishCoreAppAdmin/EnglishCoreAdmin/src/components/Students/StudentItem.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './StudentItem.css'
+
+function StudentItem(){
+    return(
+        <div className="GroupItem">
+            <p>Nombre:    {}</p>
+            <p>Nivel:     {}</p>
+            <p>Grupo:      {}</p>
+        </div>
+    )
+}
+
+export default StudentItem;
--- EnglishCoreAppAdmin/EnglishCoreAdmin/src/pages/ManageStudents.jsx
+++ EnglishCoreAppAdmin/EnglishCoreAdmin/src/pages/ManageStudents.jsx
@@ -1,12 +1,38 @@
 import React from 'react'
+import '../assets/styles/ManageStudents.css';
 import Header from '../components/Shared/Header'
 import Footer from '../components/Shared/Footer'
+import GroupItem from '../components/Students/StudentItem';
+
 function ManageStudents () {
   return (
     <div className='stud-page'>
-      <Header/>
-      Administrar estudiantes
-      <Footer/>
+      <header className="header-eng">
+        <Header />
+      </header>
+      <div className="top-bar">
+        <div>
+          <p>Ordenar por:</p>
+          <div className="custom-combobox">
+            <select>
+              <option value="1">Nivel-1</option>
+              <option value="2">Nivel-2</option>
+              <option value="3">Nivel-3</option>
+              <option value="4">Nivel-4</option>
+            </select>
+          </div>
+          
+      </div>
+      <button>Agregar Alumno</button>
+      </div>
+
+      <div className="content">
+        <GroupItem />
+      </div>
+
+      <footer className="footer-eng">
+        <Footer />
+      </footer>
     </div>
   )
 }
