2fb121e9e3c6469c80b3b9265f82853a74d3c5c2
danyloagd <daniellopezaguilera1310@gmail.com>
2025-01-08 19:16:32 -0600

    Crud
    Grupos crud solo falta poner que se pueda agregar alumnos

--- EnglishCoreAppAdmin/EnglishCoreAdmin/src/components/Groups/GroupItem.jsx
+++ EnglishCoreAppAdmin/EnglishCoreAdmin/src/components/Groups/GroupItem.jsx
@@ -1,61 +1,175 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import {UserPreview} from "../../pages/GroupView"
 import './GroupItem.css'
-function GroupItem({ Level, Hours, Days, Teacher, Students }) {
+import db from "../../services/firebaseConfig"
+import { where ,collection,getDocs,getDoc,doc,query,addDoc} from 'firebase/firestore';
+function GroupItem({Group}) {
     const [isModalVisible, setIsModalVisible] = useState(false);
+    const [students, setStudents] = useState([]);
 
+    const [FullStudents,SetFullStudents]=useState([]);
+    const [Teachers,setTeachers]=useState([]);
+
+    const [level, setLevel] = useState(null);
+    const [hours, setHours] = useState(null);
+    const [days, setDays] = useState(null);
+    const [teacher, setTeacher] = useState(null);
+    const [date,setDate]=useState(null);
+
+    const GetAllStudents= async()=>{
+        const Collection = collection(db,"users")
+        const q = query(Collection,
+            where("GroupAID","!=",Group.ID))
+        const querySnapshot = await getDocs(q);
+        const Students = []
+        querySnapshot.forEach((doc)=>{
+          const data=doc.data()
+          Students.push(new UserPreview(doc.id,data.Name+" "+data.LastName));
+        });
+        SetFullStudents(Students)
+    }
+
+    const GetTeachers = async()=>{
+        const Collection = collection(db,"users")
+        const q = query(Collection,where("IsStudent","==",false))
+        const querySnapshot = await getDocs(q);
+        const teachers = []
+        querySnapshot.forEach((doc)=>{
+          const data=doc.data()
+          teachers.push(new UserPreview(doc.id,data.Name+" "+data.LastName));
+        });
+        setTeachers(teachers)
+      }
+
+    const AddStudent = async(event)=>{
+        const value = event.target.value;
+        const user = FullStudents.find((student) => student.FName === value);
+        if(user==undefined){
+            return
+        }
+        Group.StudentList.push(user);
+        const updatedFullStudents = FullStudents.filter((student) => student.ID !== user.ID);
+        SetFullStudents(updatedFullStudents);
+        const studentsList = [...students, user]; 
+        setStudents(studentsList);
+        
+    }
+    useEffect(() => {
+        const Setup = async () => {
+          if (isModalVisible) {
+            const fetchedStudents = await Group.GetStudents();
+            setStudents(fetchedStudents); // Asegúrate de que se establecen los datos reales
+            GetAllStudents();
+            GetTeachers();
+          }
+        };
+    
+        Setup(); // Llamada a la función asíncrona
+      }, [isModalVisible,setStudents]); // Dependencias
+    // Depuración: useEffect que reacciona a isModalVisible
+    console.log('Componente montado');
     return (
         <div className="GroupItem">
-            <p>Nivel: {Level}</p>
-            <p>Horario: {Hours}</p>
-            <p>Dias: {Days}</p>
-            <p>Profesor: {Teacher}</p>
-            <p>Numero de alumnos: {Students}</p>
+            <p>Nivel: {Group.Level}</p>
+            <p>Horario: {Group.Hours}</p>
+            <p>Dias: {Group.Days}</p>
+            <p>Profesor: {Group.Profesor}</p>
+            <p>Numero de alumnos: {Group.Number}</p>
             <button onClick={() => setIsModalVisible(true)}>Desplegar</button>
             {isModalVisible && (
-                <Modal onClose={() => setIsModalVisible(false)}>
+                <Modal onClose={() =>{ 
+                    setIsModalVisible(false)
+                    setLevel(null)
+                    setHours(null)
+                    setDays(null)
+                    setTeacher(null)
+                    setDate(null)
+                }}>
                     <h2 className="modal-title">Grupo</h2>
                     <div className="modal-body">
                     <div className="form-grid">
         <div className="form-group">
             <label>Nivel:</label>
-            <select>
-                <option>Nivel-1</option>
-                <option>Nivel-2</option>
+            <select onChange={
+              (event) => {
+                const selectedValue = event.target.value;
+                setLevel(selectedValue); 
+            }} value={level||""}>
+            <option value="" disabled></option>
+                <option value={"level1"}>Nivel-1</option>
+                <option value={"level2"}>Nivel-2</option>
+                <option value={"level3"}>Nivel-3</option>
+                <option value={"level4"}>Nivel-4</option>
+                <option value={"level5"}>Nivel-5</option>
             </select>
         </div>
+
         <div className="form-group">
             <label>Horario:</label>
-            <select>
-                <option>5:00 - 8:00</option>
-                <option>8:00 - 11:00</option>
+            <select onChange={
+              (event) => {
+                const selectedValue = event.target.value;
+                setHours(selectedValue); 
+            }} value={hours||""}>
+                <option value="" disabled></option>
+                <option value={"5:00 - 8:00"}>5:00 - 8:00</option>
+                <option value={"8:00 - 11:00"}>8:00 - 11:00</option>
             </select>
         </div>
         <div className="form-group">
             <label>Días:</label>
-            <select>
-                <option>L-M-J</option>
-                <option>M-W-F</option>
+            <select onChange={(event) => {
+            const selectedValue = event.target.value;
+            setDays(selectedValue); }} 
+              value={days||""}>
+                <option value="" disabled></option>
+                <option value={"L-M-V"}>L-M-V</option>
+                <option value={"M-J"}>M-J</option>
+                <option value={"S"}>S</option>
             </select>
         </div>
         <div className="form-group">
             <label>Profesor:</label>
-            <input type="text" />
+            <select onChange={(event) => {
+            const selectedValue = event.target.value;
+            setTeacher(selectedValue);}}
+              value={teacher || ""}>
+                <option value="" disabled></option>
+              {
+                Teachers.map((teacher)=>(<option value={teacher.ID}>
+                  {teacher.FName}
+                </option>))
+              }
+            </select>
         </div>
         <div className="form-group">
             <label>Fecha de inicio:</label>
-            <input type="date" />
+            <input type="date" value={date || ""} onChange={(event) => {
+            const selectedValue = event.target.value;
+            setDate(selectedValue);}}/>
         </div>
         <div className="form-group">
             <label>Agregar Alumno:</label>
-            <input type="text" />
+            <input type="text" list="options" onInput={AddStudent}/>
+            <datalist id="options">
+                {
+                    FullStudents.map((student)=>(
+                        <option value={student.FName}></option>
+                    ))
+                }
+            </datalist>
         </div>
     </div>
     <div className="students-list">
-        <p>Daniel Lopez Aguilera - 1234567890 ✖</p>
-        <p>Daniel Lopez Aguilera - 1234567890 ✖</p>
-        <p>Daniel Lopez Aguilera - 1234567890 ✖</p>
+        {
+            students.map((student) => (
+                <p>{student.FName}</p>
+              ))
+        }
     </div>
-    <button className="submit-btn">Cerrar</button>
+    <button className="submit-btn"  onClick={
+        () => Group.UpdateGroup(level, hours, days, teacher)
+        }>guardar</button>
                     </div>
                 </Modal>
             )}
@@ -74,8 +188,6 @@ function Modal({ onClose, children }) {
         </div>
     );
 }
-
-
 
 export default GroupItem;
 
--- EnglishCoreAppAdmin/EnglishCoreAdmin/src/pages/GroupView.jsx
+++ EnglishCoreAppAdmin/EnglishCoreAdmin/src/pages/GroupView.jsx
@@ -1,38 +1,109 @@
-import React, { useContext } from 'react';
-import '../assets/styles/GroupView.css';
+import {React,useState, useEffect } from 'react';
 
+import '../assets/styles/GroupView.css';
+import db from '../services/firebaseConfig';
 import GroupItem from '../components/Groups/GroupItem';
+import Swal from 'sweetalert2';
 import Header from '../components/Shared/Header';
 import Footer from '../components/Shared/Footer';
-import { AuthContext } from '../context/AuthContext';
+import { collection, getDocs,getDoc,doc,query,where,addDoc,updateDoc} from 'firebase/firestore';
+
 function GroupView() {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [groupData, setGroupData] = useState([]);
 
-  const {name} = useContext(AuthContext);
-  const getGreeting = () => {
-    const now = new Date(); // Obtiene la fecha y hora actuales
-    const hour = now.getHours(); // Obtiene solo la hora (0 - 23)
+  const [Teachers,setTeachers]=useState([]);
+
+  const [level, setLevel] = useState(null);
+  const [hours, setHours] = useState(null);
+  const [days, setDays] = useState(null);
+  const [teacher, setTeacher] = useState(null);
+
+  const GetGroups = async() =>{
+    const GroupsCollection = collection(db, "Groups");
   
-    if (hour >= 6 && hour < 12) {
-      return "Buenos días";
-    } else if (hour >= 12 && hour < 18) {
-      return "Buenas tardes";
-    } else {
-      return "Buenas noches";
+  // Esperamos la respuesta de getDocs
+    const groupDocs = await getDocs(GroupsCollection);
+
+
+    const GroupsList = [];
+    for (const doc of groupDocs.docs) {
+      const data = doc.data();
+      const teacher= data.Teacher
+      const TEACHER=await getDoc(teacher)
+      const TeacherData=TEACHER.data();
+      let GroupData = new Group(
+        doc.id,
+        data.LevelNumber,
+        data.Hours,
+        data.Days,
+        TeacherData.Name+" "+TeacherData.LastName, // Concatenando el nombre y apellido
+        TEACHER.id,
+        data.StudentsIDs.length
+      );
+      GroupsList.push(GroupData);
     }
-  };
+    setGroupData(GroupsList);
+  }
 
-  const groupData = [
-    { id: 1, level: 1, hours: "20:00 - 21:00", days: "L-M-J-V", teacher: "Daniel Lopez", students: 1 },
-    { id: 2, level: 2, hours: "18:00 - 19:00", days: "M-J", teacher: "Maria Perez", students: 5 },
-  ];
+  const GetTeachers = async()=>{
+    const Collection = collection(db,"users")
+    const q = query(Collection,where("IsStudent","==",false))
+    const querySnapshot = await getDocs(q);
+    const teachers = []
+    querySnapshot.forEach((doc)=>{
+      const data=doc.data()
+      teachers.push(new UserPreview(doc.id,data.Name+" "+data.LastName));
+    });
+    setTeachers(teachers)
+  }
 
+  const CreateGroup = async ()=>{
+    if(!level||!hours||!days||!teacher){
+      alert(`Rellene todos los campos para crear un grupo`);
+    }else{
+      const TeacherRef = doc(db,"users",teacher)
+      const LevelRef = doc(db,"Levels",level)
+      await addDoc(collection(db,"Groups"),{
+        Days:days,
+        LevelNumber:level,
+        Hours:hours,
+        Teacher:TeacherRef,
+        Level:LevelRef,
+        StudentsIDs:[]
+      });
+      setIsModalVisible(false)
+      const Toast = Swal.mixin({
+              toast: true,
+              position: "top-end",
+              showConfirmButton: false,
+              timer: 3000,
+              timerProgressBar: true,
+              didOpen: (toast) => {
+                toast.onmouseenter = Swal.stopTimer;
+                toast.onmouseleave = Swal.resumeTimer;
+              }
+            });
+      Toast.fire({
+        icon: "success",
+        title: "Grupo creado correctamente"
+      });
+    }
+  }
+  useEffect(() => {
+    GetGroups();
+    if(isModalVisible){
+      GetTeachers()
+    }else{
+
+    }
+  }, [isModalVisible]);
   return (
     <div className="home-page">
       <header className="header-eng">
         <Header />
       </header>
-
-      <div className="greeting-div">{getGreeting()}, {name}</div>
+      
 
       <div className="top-bar">
         <div>
@@ -67,27 +138,199 @@ function GroupView() {
             </select>
           </div>
         </div>
-        <button>Crear Grupo</button>
+        <button onClick={() => setIsModalVisible(true)}>Crear Grupo</button>
       </div>
 
       <div className="content">
-        {groupData.map((group) => (
-          <GroupItem
-            key={group.id} // Clave única
-            Level={group.level}
-            Hours={group.hours}
-            Days={group.days}
-            Teacher={group.teacher}
-            Students={group.students}
-          />
+      {groupData.map((groupItem) => (
+          <GroupItem Group={groupItem} />
         ))}
       </div>
 
       <footer className="footer-eng">
         <Footer />
       </footer>
+      {isModalVisible && (
+        <Modal onClose={() => {
+          setIsModalVisible(false)
+          setDays(null)
+          setTeacher(null)
+          setHours(null)
+          setLevel(null)}}>
+        <h2 className="modal-title">Grupo</h2>
+        <div className="modal-body">
+        <div className="form-grid">
+
+        <div className="form-group">
+        <label>Nivel:</label>
+            <select onChange={
+              (event) => {
+                const selectedValue = event.target.value;
+                setLevel(selectedValue); 
+            }} value={level||""}>
+            <option value="" disabled></option>
+                <option value={"level1"}>Nivel-1</option>
+                <option value={"level2"}>Nivel-2</option>
+                <option value={"level3"}>Nivel-3</option>
+                <option value={"level4"}>Nivel-4</option>
+                <option value={"level5"}>Nivel-5</option>
+            </select>
+        </div>
+
+        <div className="form-group">
+            <label>Horario:</label>
+            <select onChange={
+              (event) => {
+                const selectedValue = event.target.value;
+                setHours(selectedValue); 
+            }} value={hours||""}>
+                <option value="" disabled></option>
+                <option value={"5:00 - 8:00"}>5:00 - 8:00</option>
+                <option value={"8:00 - 11:00"}>8:00 - 11:00</option>
+            </select>
+        </div>
+
+        <div className="form-group">
+            <label>Días:</label>
+            <select onChange={(event) => {
+            const selectedValue = event.target.value;
+            setDays(selectedValue); }} 
+              value={days||""}>
+                <option value="" disabled></option>
+                <option value={"L-M-V"}>L-M-V</option>
+                <option value={"M-J"}>M-J</option>
+                <option value={"S"}>S</option>
+            </select>
+        </div>
+
+        <div className="form-group">
+            <label>Profesor:</label>
+            <select onChange={(event) => {
+            const selectedValue = event.target.value;
+            setTeacher(selectedValue);}}
+              value={teacher || ""}>
+                <option value="" disabled></option>
+              {
+                Teachers.map((teacher)=>(<option value={teacher.ID}>
+                  {teacher.FName}
+                </option>))
+              }
+            </select>
+        </div>
+
     </div>
+
+    <button className="submit-btn" onClick={
+      CreateGroup
+      }>Crear</button>
+  </div>
+</Modal>)}
+</div>
   );
 }
 
+function Modal({ onClose, children }) {
+  return (
+      <div className="modal-overlay" onClick={onClose} >
+          <div className="modal-content" onClick={(e) => 
+            e.stopPropagation()} 
+            style={{width:"400px"}}>
+              <button className="close-button" onClick={onClose}>
+                  &times;
+              </button>
+              {children}
+          </div>
+      </div>
+  );
+}
+
+
+class Group{
+  constructor(ID,Level,Hours,Days,Profesor,ProfesorID,Number){
+    this.ID=ID;
+    this.Level=Level;
+    this.Hours=Hours;
+    this.Days=Days;
+    this.Profesor=Profesor;
+    this.ProfesorID=ProfesorID;
+    this.Number=Number
+    this.StudentList=[];
+  }
+
+  async GetStudents(){
+    const StudentsList=[]
+    const DocRef=doc(db,"Groups",this.ID)
+    const GroupDoc=await getDoc(DocRef);
+    const Students = GroupDoc.data().StudentsIDs
+    for (const i of Students){
+      const StudentDoc=await getDoc(i);
+      const data=StudentDoc.data()
+      StudentsList.push(new UserPreview(StudentDoc.id,data.Name+" "+data.LastName))
+    }
+    return StudentsList;
+  }
+
+  async UpdateGroup(level,hours,days,profesor){
+
+    this.Level=level||this.Level
+    this.Hours=hours||this.Hours
+    this.Days=days||this.Days
+   
+
+    const DocRef=doc(db,"Groups",this.ID)
+    const GroupDoc=await getDoc(DocRef);
+
+
+    const newProfesor=profesor||this.ProfesorID
+
+    const TeacherRef=doc(db, "users",newProfesor);
+    const TeacherDoc= await getDoc(TeacherRef)
+    const TeacherData=TeacherDoc.data()
+
+    const OriginalTeacher= doc (db,"users",this.ProfesorID)
+    const OriginalDoc=await getDoc(OriginalTeacher)
+
+    this.Profesor=TeacherData.Name+" "+TeacherData.LastName
+    this.ProfesorID=TeacherDoc.id
+
+    
+
+    const LevelRef = doc(db,"Levels",this.Level)
+    const NewGroup={
+      Days:this.Days,
+      Hours:this.Hours,
+      Teacher:TeacherRef,
+      Level:LevelRef,
+      LevelNumber:this.Level
+
+    }
+    await updateDoc(DocRef,NewGroup);
+
+    if(profesor){
+      const OriginalData= OriginalDoc.data()
+      const OriginalGroups=OriginalData.Groups
+      const copy=OriginalGroups
+      copy.forEach((group,index)=>{
+        if (group.id === GroupDoc.id) {
+          OriginalGroups.splice(index, 1);
+        }
+      })
+      const NewGroups=TeacherData.Groups
+      NewGroups.push(DocRef)
+
+      await updateDoc(TeacherRef,{Groups:NewGroups})
+      await updateDoc(OriginalTeacher,{Groups:OriginalGroups})
+    }
+
+
+
+  }
+}
+
+export class UserPreview{
+  constructor(ID,FName){
+    this.ID=ID;
+    this.FName=FName;
+  }
+}
 export default GroupView;
